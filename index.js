798472684658e9e845c4c42815c6b9eae2c1c23a
onam <onamkrverma@gmail.com>
2022-10-11 14:44:02 +0530

    added env port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connectToMongo = require("./db");
 const app = express();
 const cors = require('cors')
-const port = 5000;
+const port = process.env.PORT || 5000;
 connectToMongo();
 
 app.use(express.json());
